0263944412709a6316f74a1f981e0502d177da4b
Portable User <user@example.com>
2026-03-16 11:37:45 +0100

    Ajout affichage sur page Espace membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
-use App\Entity\Proprietaire;
 use App\Repository\ProprietaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Entity\Proprietaire;
+
 final class MembreController extends AbstractController
 {
     #[Route('/membre', name: 'app_membre')]
-    public function index(): Response
+    public function index(ProprietaireRepository $repository): Response
     {
-        return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
+        $membre = $repository->findOneBy(['user' => $this->getUser()]);
+
+        if (!$membre) {
+            throw $this->createNotFoundException('Aucun membre trouvé pour cet utilisateur.');
+        }
+
+        return $this->render('membre/index.html.twig',[
+            'membre' => $membre,
         ]);
     }
 }
