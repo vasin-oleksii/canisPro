c05b8d9e9da875d380d91e69a90776d67bd0d8bf
Portable User <user@example.com>
2026-03-16 09:27:51 +0100

    Sécurité + Espace memebre + menu

---
+++ src/Controller/MembreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MembreController extends AbstractController
+{
+    #[Route('/membre', name: 'app_membre')]
+    public function index(): Response
+    {
+        return $this->render('membre/index.html.twig', [
+            'controller_name' => 'MembreController',
+        ]);
+    }
+}
