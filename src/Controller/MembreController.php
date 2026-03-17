0788eef281a05e303cfcadf5f1eba31911e5fd54
Portable User <user@example.com>
2026-03-17 11:52:48 +0100

    ajouts fonction

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -5,9 +5,14 @@ namespace App\Controller;
 use App\Repository\ProprietaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 use App\Entity\Proprietaire;
+use App\Form\ProprietaireType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 final class MembreController extends AbstractController
 {
@@ -21,6 +26,33 @@ final class MembreController extends AbstractController
         }
 
         return $this->render('membre/index.html.twig',[
+            'membre' => $membre,
+        ]);
+    }
+    
+    #[Route('/membre/{id}', name: 'app_membre_modifier',requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function modifier(ProprietaireRepository $repository, int $id, EntityManagerInterface $manager, Request $request): Response
+    {
+        $membre = $repository->find($id);
+
+        if (!$membre) {
+            throw $this->createNotFoundException('Aucun membre trouvé pour cet utilisateur.');
+        }
+
+        $form = $this->createForm(ProprietaireType::class, $membre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($membre);
+            $manager->flush();
+
+            $this->addFlash('success', 'Membre modifié.');
+
+            return $this->redirectToRoute('app_membre');
+        }
+
+        return $this->render('membre/modifMembre.html.twig', [
+            'form' => $form->createView(),
             'membre' => $membre,
         ]);
     }
